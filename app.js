d9f719a5a59a9672ea9003b1d99352942176e6ce
Dominik Zwoliński <117155092+Domun335@users.noreply.github.com>
2025-05-15 00:01:24 +0200

    Update 05
    - updated gallery
    - added lightbox

--- app.js
+++ app.js
@@ -19,3 +19,51 @@ function setLanguage(lang) {
 
 const savedLang = localStorage.getItem('lang') || 'pl'
 setLanguage(savedLang)
+
+async function fetchImagesJson(url) {
+  try {
+    const response = await fetch(url)
+    if (!response.ok) throw new Error('Nie udało się załadować pliku JSON')
+    return await response.json()
+  } catch (error) {
+    console.error('Błąd podczas pobierania obrazów:', error)
+    return []
+  }
+}
+
+function setGallery(imagesNames) {
+  const galleryDiv = document.getElementById('gallery')
+  const lightbox = document.getElementById('lightbox')
+  const lightboxImg = document.getElementById('lightbox-img')
+  const lightboxClose = document.getElementById('lightbox-close')
+
+  imagesNames.forEach((file) => {
+    const img = document.createElement('img')
+    img.src = `images/gallery/${file}`
+    img.alt = file
+    img.classList.add('main__gallery-image')
+    img.style.cursor = 'pointer'
+    galleryDiv.appendChild(img)
+
+    img.addEventListener('click', () => {
+      lightboxImg.src = img.src
+      lightbox.classList.remove('hidden')
+    })
+  })
+
+  lightboxClose.addEventListener('click', () => {
+    lightbox.classList.add('hidden')
+    lightboxImg.src = ''
+  })
+
+  lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+      lightbox.classList.add('hidden')
+      lightboxImg.src = ''
+    }
+  })
+}
+
+fetchImagesJson('/images.json').then((imageFiles) => {
+  setGallery(imageFiles)
+})
